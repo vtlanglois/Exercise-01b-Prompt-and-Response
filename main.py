4d47dda04f7bdcdb8ad59496c05906e69bd26901
BL-MSCH-C220-F19 <53633703+BL-MSCH-C220-F19@users.noreply.github.com>
2021-08-26 12:39:30 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import sys
+assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
+
+passage_name = "West of House"
+passage_text = "This is an open field west of a white house, with a boarded front door."
