65efe7dba92c1f47c753c47f2beb718e5f3d962a
Langlois, Vince Thomas <vtlanglo@iu.edu>
2021-08-26 22:23:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
 
 passage_name = "West of House"
 passage_text = "This is an open field west of a white house, with a boarded front door."
+print("You are at the " + passage_name)
+print(passage_text)
+reply = input("What would you like to do? ")
+if(reply == "go north"):
+    print("Good job!")
